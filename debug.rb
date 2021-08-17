bfe62699e0ab03dac91dd8c7de8a4f52911ed40d
Chris Waters <chris.waters@cutover.com>
2021-08-17 10:58:51 +0100

    simpler

--- debug.rb
+++ debug.rb
@@ -1,20 +1,17 @@
 require 'nokogiri'
 require 'pry'
 
-xml = File.open("xml/5.xml") { |f| Nokogiri::XML(f) }
+xml = File.open("xml/results4.xml") { |f| Nokogiri::XML(f) }
 seed = xml.xpath('//property').attr('value')
 tests = xml.xpath('//testcase').each_with_object([]) do |testcase, tests|
   tests << testcase.attr('file')
-end
+end.uniq
 
-failure = "gocardless_migrator"
-failing_test = tests.uniq.detect { _1.include?(failure) }
-failing_index = tests.uniq.index(failing_test)
+failure = "query_spec"
+failing_test = tests.detect { _1.include?(failure) }
+failing_index = tests.index(failing_test)
 
-strategies = [
-  { operator: :>=, value: 106 },
-  { operator: :<, value: 110 }
-]
+strategies = []
 
 tests_to_run = tests.uniq.select.with_index do |test, i|
   strategies.all? do |strategy|
@@ -24,4 +21,5 @@ end
 
 tests_to_run << failing_test
 
-puts "bin/rspec #{tests_to_run.uniq.join(' ')} --seed #{seed}"
+puts "total tests: #{tests[0..failing_index].size}"
+puts "bin/rspec #{tests[0..failing_index].join(' ')}"
